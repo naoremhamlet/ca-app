95c86898ebcc38d8fed1f9f0a8afd5416a32cd3e
Naorem Hemlet <naoremhamlet@gmail.com>
2022-06-10 07:10:33 +0530

    new

--- src/helper/thumbnail.js
+++ src/helper/thumbnail.js
@@ -1,11 +1,27 @@
-import generatePdfThumbnails from 'pdf-thumbnails-generator';
+// import generatePdfThumbnails from 'pdf-thumbnails-generator';
+import PdfThumbnail from 'react-pdf-thumbnail';
 
 export default async function thumbnail(file) {
-    try {
-        const thumbnails = await generatePdfThumbnails(file, 100);
-        return thumbnails[0].thumbnail
-    } catch (err) {
-        console.error(err);
+    // try {
+    //     const thumbnails = await generatePdfThumbnails('https://cors-anywhere.herokuapp.com/'+file, 100);
+    //     return thumbnails[0].thumbnail
+    // } catch (err) {
+    //     console.error(err);
+    // }
+    // return null;
+    console.log(file)
+    const { error, imageUrl } = await PdfThumbnail(
+        file,
+        {
+            fileName: 'mythumbimage.png',
+            height: 200,
+            width: 200,
+            pageNo: 1
+        }
+    );
+    if (!error) {
+        return imageUrl;
+    } else {
+        return null;
     }
-    return null;
 }
